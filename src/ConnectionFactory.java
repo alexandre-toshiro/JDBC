29f6a65c18c00c83c0c6b1dc51da987612a9a721
alexandre-toshiro <toshiro.lyn@gmail.com>
2021-06-15 21:25:37 -0300

    Criando um pool de conexões

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,13 +1,27 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	
+	public DataSource dataSource;//interface do java que expões as info do pool
+
+	public ConnectionFactory() {
+		
+		ComboPooledDataSource comboPool = new ComboPooledDataSource();
+		comboPool.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPool.setUser("root");
+		comboPool.setPassword("123456");
+		
+		this.dataSource = comboPool;//Expõe as configuração de conexão.
+	}
+
 	public Connection recuperarConexao() throws SQLException {
 
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-				"root", "123456");
-		//abstraindo a criaçõ de conex]ao para uma classe a parte.
+		return this.dataSource.getConnection(); //retorna a conexão que está disponível no pool de conexões.
 	}
 
 }
--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -9,24 +9,23 @@ public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		connection.setAutoCommit(false);// JDBC n controla mais a transação.Devemos fazer o controle
+		try (Connection connection = factory.recuperarConexao()) {//try-with-resources
+			connection.setAutoCommit(false);// JDBC n controla mais a transação.Devemos fazer o controle
 
-		try {
-			PreparedStatement stm = connection.prepareStatement // Passamos a responsabilidade para o JDBC
-			("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES(?,?)", // os parâmetros são interrogações
-					Statement.RETURN_GENERATED_KEYS);
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);) {
 
-			adicionarVariavel("SmartTV", "45 polegadas", stm);
-			adicionarVariavel("Radio", "Radio da bateria", stm);
+				adicionarVariavel("SmartTV", "45 polegadas", stm);
+				adicionarVariavel("Radio", "Radio da bateria", stm);
 
-			connection.commit();
-			stm.close();
-			connection.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
+				connection.commit();
+				stm.close();
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
 	}
 
@@ -34,22 +33,21 @@ public class TestaInsercaoComParametro {
 		stm.setString(1, nome);// seta no 1º atributo a variavel nome
 		stm.setString(2, descricao);
 
-		if(nome.equals("Radio")) {
+		if (nome.equals("Radio")) {
 			throw new RuntimeException("Não foi possível adicionar o produto");
 		}
 
 		stm.execute();
 
-		ResultSet rst = stm.getGeneratedKeys();
-
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O ID criado foi: " + id);
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O ID criado foi: " + id);
+			}
 		}
-		rst.close();
-
 		// dessa forma evitamos erros de compilação/interpretação caso o usuário insira
 		// aspas simples, sql injection
+		//Utilizando o try-with-resources, não precisamos nos preocupar em ficar fechando os recursos, pos será fechado de forma automática.
 	}
 
 }
