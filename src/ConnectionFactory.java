47c0559b0106a6aeb8318e9715714948bc59a8d5
alexandre-toshiro <toshiro.lyn@gmail.com>
2021-06-15 21:41:54 -0300

    Testando o pool de conexoes

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -6,22 +6,24 @@ import javax.sql.DataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
-	
-	public DataSource dataSource;//interface do java que expões as info do pool
+
+	public DataSource dataSource;// interface do java que expões as info do pool
 
 	public ConnectionFactory() {
-		
+
 		ComboPooledDataSource comboPool = new ComboPooledDataSource();
 		comboPool.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
 		comboPool.setUser("root");
 		comboPool.setPassword("123456");
 		
-		this.dataSource = comboPool;//Expõe as configuração de conexão.
+		comboPool.setMaxPoolSize(15);// define o máximo de conexões disponíveis
+		// já carrega o pool com 15 conexões disponíveis.
+
+		this.dataSource = comboPool;// Expõe as configuração de conexão.
 	}
 
 	public Connection recuperarConexao() throws SQLException {
 
-		return this.dataSource.getConnection(); //retorna a conexão que está disponível no pool de conexões.
+		return this.dataSource.getConnection(); // retorna a conexão que está disponível no pool de conexões.
 	}
-
 }
--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -12,5 +12,4 @@ public class TestaConexao {
 
 		connection.close();
 	}
-
 }
